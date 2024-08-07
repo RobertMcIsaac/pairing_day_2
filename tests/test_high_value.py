e3a2f92b7f3787fa11b5ec0b4c5315340df0c0de
Sarah Peel <sarahpeel93@hotmail.co.uk>
2024-08-07 10:47:14 +0100

    Pairing Challenge Complete

--- tests/test_high_value.py
+++ tests/test_high_value.py
@@ -1,4 +1,4 @@
-from lib.high_value import *
+from lib.high_value import HighValue
 
 def test_highvalue_value_first_is_2():  
     high_value_1 = HighValue(2, 3)
@@ -16,3 +16,14 @@ def test_highvalue_first_value_is_higher():
 def test_highvalue_second_value_is_higher():
     high_value_1 = HighValue(2, 3)
     assert high_value_1.get_highest() == "Second value is higher"
+
+def test_highvalue_both_values_are_equal():
+    high_value_1 = HighValue(2, 2)
+    assert high_value_1.get_highest() == "Values are equal"
+
+"""
+testing the add method against two arguments
+"""
+def test_highvalue_add_method():
+    high_value_1 = HighValue(2, 3)
+    assert high_value_1.add(1, "second") == 4
